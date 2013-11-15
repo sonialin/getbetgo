afccf509179a447d04a57935e8935628738c5357
Sonia Lin <sonia_lin@hotmail.com>
2013-11-15 19:52:42 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Getbetgo::Application.config.secret_key_base = '46de66cc3d14c3dfe75bc04aa3ec56e5eb35e16708f099057deff97341e8c70d75b2e5ceb96fe59181051b4158679dac6f04f7aad979953c3e8d8fdb4e2b6a88'
