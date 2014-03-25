bddf8207eb96282ee43a040c726a990cc6949df9
Sonia Lin <sonia_lin@hotmail.com>
2014-03-26 03:05:36 +0800

    Change homepage listing layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :funds
   has_many :paypal_recipient_accounts
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/100x100"
+  has_attached_file :avatar, :styles => { :medium => "200x200#", :thumb => "50x50#" }, :default_url => "http://placehold.it/100x100"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   validates :name, presence: true
