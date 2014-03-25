adca51151301d7940f6df4368599372fafd33bb4
Sonia Lin <sonia_lin@hotmail.com>
2014-03-25 08:38:00 +0800

    Enable user to upload avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,8 @@ class User < ActiveRecord::Base
   has_many :funds
   has_many :paypal_recipient_accounts
 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/100x100"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   validates :name, presence: true
 end
