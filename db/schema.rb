6772c7363d4f7ab0c5e22182c8444a6b9abae370
Sonia Lin <sonia_lin@hotmail.com>
2014-04-17 00:57:13 +0200

    Add title field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140413214733) do
+ActiveRecord::Schema.define(version: 20140416223355) do
 
   create_table "bets", force: true do |t|
     t.integer  "post_id"
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(version: 20140413214733) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "title"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
