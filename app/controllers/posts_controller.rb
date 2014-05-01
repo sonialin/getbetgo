ca5493e73ba5090e121dd34b1890615868bf173e
Sonia Lin <sonia_lin@hotmail.com>
2014-05-01 22:51:37 +0200

    Add location and search autocomplete to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -111,6 +111,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :description, :image, :image_url, :price, :quantity, :tag_list, :category)
+      params.require(:post).permit(:title, :description, :image, :image_url, :price, :quantity, :tag_list, :category, :location)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501111429) do
+ActiveRecord::Schema.define(version: 20140501201618) do
 
   create_table "bets", force: true do |t|
     t.integer  "post_id"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20140501111429) do
     t.string   "status"
     t.string   "slug"
     t.string   "category"
+    t.string   "location"
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
