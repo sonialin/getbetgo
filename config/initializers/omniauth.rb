c8cd160fa5d506a9ccb62d7a526000cc52428464
Sonia Lin <sonia_lin@hotmail.com>
2014-06-21 11:55:35 +0800

    Move facebook config back to devise intializer

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["OAUTH_FACEBOOK_APPID_DEV"], ENV["OAUTH_FACEBOOK_APPSECRET_DEV"], {
-    secure_image_url: 'true',
-    image_size: 'square'
-  }
-end
