8ac5e837f4d3d97df9024e20a3c5329cc774eefa
Sonia Lin <sonia_lin@hotmail.com>
2014-06-26 17:21:15 +0800

    Apply layout on finances page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,9 @@ class PagesController < ApplicationController
 
   def finances
   	@user = current_user
-  	@bets = @user.bets
-  	@paypal_recipient_account = PaypalRecipientAccount.new
+  	@bets = @user.bets.order('created_at desc')
+    @posts = @user.posts.order("created_at desc")
+    @credit_sum = @bets.inject(0) {|sum, bet| sum + bet.post.price}
+  	@paypal_recipient_account = PaypalRecipientAccount.new  
   end
 end
