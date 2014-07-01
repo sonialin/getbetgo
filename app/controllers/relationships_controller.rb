783e0fb5483be26e01eff4b2b246f2f465edc5cf
Sonia Lin <sonia_lin@hotmail.com>
2014-07-01 11:46:02 +0800

    Add notification upon following user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,6 +7,10 @@ class RelationshipsController < ApplicationController
 		@relationship.follower = current_user
 
 		if @relationship.save
+			@relationship.create_activity :create, owner: @relationship.follower, recipient: @relationship.followed
+    	@relationship.followed.notify("#{@relationship.follower.name} just followed you", 
+    																"#{@relationship.follower.name} just followed you",
+                          					notified_object = @relationship)
 			redirect_to :back
 		else
 			flash[:error] = "Oops, something went wrong."
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ActiveRecord::Base
 	belongs_to :follower, class_name: 'User'
  	belongs_to :followed, class_name: 'User'
+
+ 	include PublicActivity::Common
 end
