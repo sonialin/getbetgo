7d4f397cb916251f07bb7116c777466e887c202e
Sonia Lin <sonia_lin@hotmail.com>
2014-07-04 16:18:14 +0800

    Delete import avatar to user model from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,6 @@ class User < ActiveRecord::Base
                             provider:auth.provider,
                             uid:auth.uid,
                             email:auth.info.email,
-                            avatar: auth.info.image,
                             password:Devise.friendly_token[0,20]
                           )
       end
