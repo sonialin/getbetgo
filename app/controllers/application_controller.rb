70a7f5e4661248b952c0f61a7d401f816dd1b88b
Sonia Lin <sonia_lin@hotmail.com>
2014-07-04 21:18:42 +0800

    Apply layout for edit user page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def after_sign_up_path_for(resource)
-	  session[:previous_url] || root_path
+	  root_path
 	end
 
 	def after_sign_in_path_for(resource)
