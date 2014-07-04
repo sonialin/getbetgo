e35c9084b303e60ceb79a5c16df7bb2ab5d202ed
Sonia Lin <sonia_lin@hotmail.com>
2014-07-04 15:39:18 +0800

    Add avatar to user_info and change all views to the new avatar

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -2,4 +2,5 @@ class UserInfo < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :cover_photo, :styles => { :big => '600', :medium => "400x200#" }
+	has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "http://placehold.it/100x100"
 end
