01715f6325e541c7e52fe1776ee75d4881b4355b
Sonia Lin <sonia_lin@hotmail.com>
2014-07-04 20:31:07 +0800

    Fix registraion layout and redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,15 @@ class ApplicationController < ActionController::Base
 	  # store last url - this is needed for post-login redirect to whatever the user last visited.
 	  if (request.fullpath != "/users/sign_in" &&
 	      request.fullpath != "/users/sign_up" &&
-	      request.fullpath != "/users/password" &&
+	      !request.fullpath.include?("/users/password") &&
 	      request.fullpath != "/users/sign_out" &&
 	      !request.xhr?) # don't store ajax calls
 	    session[:previous_url] = request.fullpath 
 	  end
+	end
+
+	def after_sign_up_path_for(resource)
+	  session[:previous_url] || root_path
 	end
 
 	def after_sign_in_path_for(resource)
