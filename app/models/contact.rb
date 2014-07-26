7ee7c172912f95727066c365b3df8b146df22cbf
Sonia Lin <sonia_lin@hotmail.com>
2014-07-26 17:45:51 +0800

    Add contact form modal

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,8 +8,8 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
-      :to => "your_email@example.org",
+      :subject => "Someone Wrote to FundWok",
+      :to => "fundwok@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
