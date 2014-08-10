8130a4e12a9d0490dd49137ee873bf06d00980f1
Sonia Lin <sonia_lin@hotmail.com>
2014-08-10 17:01:07 +0800

    Add confirmable to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140810084243) do
+ActiveRecord::Schema.define(version: 20140810085806) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -313,8 +313,13 @@ ActiveRecord::Schema.define(version: 20140810084243) do
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
