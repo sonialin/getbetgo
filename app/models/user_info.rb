06eb86231f52acacfc1c4b55c425ecfd06e0247d
Sonia Lin <sonia_lin@hotmail.com>
2014-08-16 20:39:19 +0800

    Apply layout to user info hover

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -2,5 +2,6 @@ class UserInfo < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :cover_photo, :styles => { :big => '600', :medium => "400x200#" }
-	has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "http://placehold.it/100x100"
+	has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, 
+										:default_url => "http://res.cloudinary.com/dxytnnzk9/image/upload/v1408186172/fw-avatar-grey_hx1dgp.png"
 end
