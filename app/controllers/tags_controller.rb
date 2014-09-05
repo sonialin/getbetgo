724859920845824ce2be29584db941ff0e465abc
Nishant Dalmia <nishant1851@gmail.com>
2014-09-05 22:35:52 +0530

    tags prediction in fund form

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,7 @@
+class TagsController < ApplicationController
+  def prediction
+    keyword = "%#{params[:q].upcase}%"
+    predictions = ActsAsTaggableOn::Tag.where("UPPER(name) LIKE ?",keyword).limit(10).order("name").select("tags.id as id, tags.name as name")
+    render :json => predictions, :status => 200
+  end
+end
