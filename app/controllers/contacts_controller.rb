5c77fa3c736ebf0de1a67763bca1aec85e18fa9a
Sonia Lin <sonia_lin@hotmail.com>
2014-09-28 14:22:46 -0500

    Add header and footer to error template

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      
     else
       flash.now[:error] = 'Oops - something went wrong. Please try again.'
       render :new
