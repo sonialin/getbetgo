f8d6864b14e5a1b16b6748af2cc4baa6cf280fbe
Nishant Dalmia <nishant1851@gmail.com>
2014-10-03 02:00:37 +0530

    added region model

---
+++ db/migrate/20140930194431_create_regions.rb
@@ -0,0 +1,11 @@
+class CreateRegions < ActiveRecord::Migration
+  def change
+    create_table :regions do |t|
+      t.string :name
+      t.integer :country_id
+      t.timestamps
+    end
+
+    add_index :regions, :name
+  end
+end
